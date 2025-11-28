51876d49d1ed0eade8d906e305248654d7042614
TRIANA_TECHNOLOGY <andresfelipetriana443@gmail.com>
2025-11-27 19:34:06 -0500

    se areglo algo de una vista

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,12 +1,5 @@
-import { Html, useProgress } from "@react-three/drei";
-
 const Loader = () => {
-  const { progress } = useProgress();
-  return (
-    <Html center className="text-xl font-normal text-center">
-      {progress}% Loaded
-    </Html>
-  );
+  return null;
 };
 
 export default Loader;
